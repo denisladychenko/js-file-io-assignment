38a7283f3df020fbfe90bb713f2f7e7ac88cd9a8
denisladychenko <denisladychenko@gmail.com>
2017-09-05 09:20:51 -0500

    input output

--- Employee.js
+++ Employee.js
@@ -3,14 +3,40 @@
 const fs = require('fs')
 
 class Employee {
-  constructor (/* ??? */) {
+  
+//constructor for the class
+  constructor(obj) {
     // TODO
+    this.name = obj.name;
+    this.title = obj.title;
+    this.salary = obj.salary;
+   }
+   //parses data from the specified directory
+  static parseFromFilePath(dir){
+    const unpData = fs.readFileSync(dir);
+    const empObj = JSON.parse(unpData);
+    return new Employee(empObj);
   }
 
+//changes title and salary of the employee object
+  promote(title, salary){
+    setTitle(this, title);
+    setSalary(this, salary);
+   
+  }
+
+ 
   // TODO ???
 }
 
-// TODO ???
+// set title for the object
+const setTitle = (obj, title) => {
+  obj.title = title;
+}
+//sets salary for the object
+const setSalary = (obj, salary) => {
+  obj.salary = salary;
+}
 
 module.exports = {
   Employee
